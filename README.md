076a897dd534aaae9a85f88a90cc22475706aa76
Jonas Günner <jonas@exploitox.de>
2023-04-04 13:02:15 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# PS-AddADUsers-CSV
+# Verwendung des Scripts
+
+Verwende die template.csv als Vorlage. Aufgebaut ist die Vorlage wiefolgt: 
+
+```Vorname, Nachname, Initialien, Benutzername, Passwort, E-Mail, Abteilung und OU```
+
+Alle Werte bis auf dem Benutzernamen und der OU sind optionale Werte.
+Wenn das Feld 'Passwort' leer gelassen wird, generiert das Script einen 12 Zeichen langes Passwort.
+Die Länge der Passwörter kann durch das Argument ```'-PasswordLenth'``` verändert werden.
+
+Den Wert 'OU' bestimmt den Anlageort des Benutzers. Die verfügbaren OUs können durch den folgenden PowerShell-Befehl aufgelistet werden:
+
+```powershell
+Get-ADObject -Filter { ObjectClass -eq 'organizationalunit' }
+```
+
+
+## Syntax
+
+```powershell
+.\script.ps1 -csvfile <Pfad zur CSV-Datei> -Domain <Lokale Domain>
+```
+
+## Beispiele
+
+### PowerShell Script
+```powershell
+.\script.ps1 -csvfile .\template.csv -Domain vnext.local
+```
+
+```powershell
+.\script.ps1 -csvfile .\template.csv -Domain vnext.local -PasswordLengh 18
+```
+
+### Template
+<img src="assets/Beispiel.png"></img>
---
+++ script.ps1
@@ -0,0 +1,74 @@
+﻿param (
+    [string]$domain = $( Read-Host "Domain" ),
+    [string]$csvfile = $( Read-Host "Pfad zur CSV-File" ),
+    [int]$PasswordLenght = 12
+)
+
+Function GenerateStrongPassword ([Parameter(Mandatory=$true)][int]$PasswordLenght)
+{
+    Add-Type -AssemblyName System.Web
+    $PassComplexCheck = $false
+    do {
+        $newPassword=[System.Web.Security.Membership]::GeneratePassword($PasswordLenght,1)
+        If ( ($newPassword -cmatch "[A-Z\p{Lu}\s]") `
+        -and ($newPassword -cmatch "[a-z\p{Ll}\s]") `
+        -and ($newPassword -match "[\d]") `
+        -and ($newPassword -match "[^\w]")
+        )
+        {
+            $PassComplexCheck=$True
+        }
+    } While ($PassComplexCheck -eq $false)
+    return $newPassword
+}
+
+# Importiere Powershell Module
+Import-Module ActiveDirectory
+
+# Lade CSV-Datei in Array
+$ADUsers = Import-Csv $csvfile
+
+# Loope durch Array (CSV)
+foreach ($User in $ADUsers) {
+    $Benutzername = $User.Benutzername
+
+    if (Get-ADUser -F { SamAccountName -eq $Benutzername }) {
+        Write-Warning "Benutzer $Benutzername existiert bereits in der Active Directory Domäne '$domain'."
+    }
+    else {
+        # Definiere alle Werte in eigene Variablen
+        $Vorname = $User.Vorname
+        $Nachname = $User.Nachname
+        $Initialen = $User.Initialen
+        if (!$User.Passwort) {
+            $Passwort = GenerateStrongPassword -PasswordLenght $PasswordLenght
+            Write-Host "Passwort für Benutzer '$Benutzername' wurde automatisch generiert: $Passwort" -ForegroundColor Blue
+        }
+        else {
+            $Passwort = $User.Passwort
+        }
+        $Email = $User.Email
+        $Abteilung = $User.Abteilung
+        $OU = $User.OU
+
+        New-ADUser `
+        -SamAccountName $Benutzername `
+        -Name "$Vorname $Nachname" `
+        -GivenName $Vorname `
+        -Surname $Nachname `
+        -Initials $Initialen `
+        -DisplayName "$Vorname $Nachname" `
+        -UserPrincipalName "$Benutzername@$domain" `
+        -AccountPassword (ConvertTo-SecureString $Passwort -AsPlainText -Force) `
+        -Enabled $true `
+        -ChangePasswordAtLogon $false `
+        -PasswordNeverExpires $true `
+        -Path $OU `
+        -EmailAddress $Email `
+        -Department $Abteilung
+
+        Write-Host "Benutzer $Benutzername wurde erfolgreich in der Active Directory Domäne '$domain' erstellt." -ForegroundColor Green
+    }
+}
+
+Write-Host "Vorgang abgeschlossen."
